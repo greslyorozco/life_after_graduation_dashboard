f9a7526fcb162ca13694ea36b4002e55c3c928d6
greslyorozco <orozcog3@southernct.edu>
2026-05-07 12:50:05 -0400

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+st.title("Life After Graduation")
+st.write("This dashboard explores student debt, salary, and employment outcomes after college.")
