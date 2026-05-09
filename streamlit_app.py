f22b2e198b41b9eccf055df90a0cb121d9a40336
Elena-T26 <lanieitaly12@gmail.com>
2026-05-09 10:55:48 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -405,16 +405,16 @@ with tab5:
     """)
 with tab6:
     st.header("Key Findings")
+    st.write("test")
 
-    st.success(
-    """
-    - Higher student debt does not always lead to higher salaries.
-    - Major choice strongly impacts earnings after graduation.
-    - Geographic location may influence debt levels and career outcomes.
-    - Public schools generally showed lower median debt compared to some private institutions.
-    - Salary and employmnet outcomes should both be considered when evaluating college value.
-    """
-)
+    #st.success("""
+   # - Higher student debt does not always lead to higher salaries.
+   # - Major choice strongly impacts earnings after graduation.
+  #  - Geographic location may influence debt levels and career outcomes.
+   # - Public schools generally showed lower median debt compared to some private institutions.
+   # - Salary and employmnet outcomes should both be considered when evaluating college value.
+    #"""
+#)
 
 with tab7:
     st.header("Conclusion")
