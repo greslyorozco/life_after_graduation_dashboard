92861c7592a891ca020e98e6b603e2a11063aa44
Elena-T26 <lanieitaly12@gmail.com>
2026-05-09 10:52:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,15 +87,17 @@ selected_major_category = st.sidebar.selectbox(
     "Major category",
     major_categories
 )
-salary_min = int(majors_df['Median'].min())
-salary_max = int(majors.df['Median'].max())
-salary_range = st.sidebar.slider(
-    "Select Median Salary Range",
-    salary_min,
-    salary_max,
-    (salary_min,salary_max)
-)
-
+#salary_min = int(majors_df['Median'].min())
+#salary_max = int(majors.df['Median'].max())
+#salary_range = st.sidebar.slider(
+  #  "Select Median Salary Range",
+ #   salary_min,
+ #   salary_max,
+ #   (salary_min,salary_max)
+#)
+majors_filtered = majors_df.loc[
+    majors_df['Major_category'] == selected_major_category
+]
 majors_filtered = majors_df.loc[
     (majors_df['Major_category'] == selected_major_category) &
     (majors_df['Median'] >= salary_range[0]) &
