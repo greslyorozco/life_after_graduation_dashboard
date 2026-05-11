8e1db06153a95cf4f9a1b64623b68b7b6ebf0cf2
Elena-T26 <lanieitaly12@gmail.com>
2026-05-11 01:32:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -238,7 +238,7 @@ with tab2:
                 color_continuous_scale=["lightblue", "gold", "salmon"]
             )
         
-        fig.update_xaxes(tickformat=".0%")
+        #fig.update_xaxes(tickformat=".0%")
         fig.update_traces(
             hovertemplate="<b>%{y}</b><br>Unemployment Rate: %{x:.1%}<extra></extra>"
         )
