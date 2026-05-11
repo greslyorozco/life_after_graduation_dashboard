951ac92b2199c43d9ebc44c3a76676a4e155dc85
Elena-T26 <lanieitaly12@gmail.com>
2026-05-11 01:26:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -240,7 +240,7 @@ with tab2:
         
         fig2.update_xaxes(tickformat=".0%")
         fig2.update_traces(
-            hovertemplate="<b>%{y}</b><br>Unemployment Rate: %{x:.1%}<extra></extra>
+            hovertemplate="<b>%{y}</b><br>Unemployment Rate: %{x:.1%}<extra></extra>"
         )
 
         fig.update_layout(
