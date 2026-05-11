720c05b503fbd258c4a27b3ed4fdda2d000ea2a6
Elena-T26 <lanieitaly12@gmail.com>
2026-05-10 20:01:49 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -367,6 +367,9 @@ with tab5:
     st.subheader("Major Outcomes Dataset Preview")
     st.dataframe(majors_df.head(), width=1200, height=300)
 
+    st.subheader("Student Loan by State Dataset Preview")
+    st.dataframe(loan_state_df.head(), width=1000, height=300)
+
     col1, col2, col3 = st.columns(3)
 
     with col1:
