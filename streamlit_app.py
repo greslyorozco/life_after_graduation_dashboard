5417ec556ce4b5b508df4875a060a606c1243732
Elena-T26 <lanieitaly12@gmail.com>
2026-05-11 00:56:24 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -478,37 +478,37 @@ with tab5:
     
     col1, col2, col3 = st.columns(3)
     
-with col1:
-    st.metric("Scorecard Rows", scorecard_df.shape[0])
+    with col1:
+        st.metric("Scorecard Rows", scorecard_df.shape[0])
 
-with col2:
-    st.metric("Scorecard Columns", scorecard_df.shape[1])
+    with col2:
+        st.metric("Scorecard Columns", scorecard_df.shape[1])
 
-with st.expander("Click to see Scorecard columns"):
-    st.write(scorecard_df.columns.tolist())
+    with st.expander("Click to see Scorecard columns"):
+        st.write(scorecard_df.columns.tolist())
 
-with st.expander("Click to see missing values"):
-    st.write(scorecard_df.isna().sum())
+    with st.expander("Click to see missing values"):
+        st.write(scorecard_df.isna().sum())
 
-    st.subheader("Major Outcomes Dataset Preview")
-    st.dataframe(majors_df.head(), width=1200, height=300)
+        st.subheader("Major Outcomes Dataset Preview")
+        st.dataframe(majors_df.head(), width=1200, height=300)
 
     col1, col2, col3 = st.columns(3)
 
-with col1:
-    st.metric("Majors Rows", majors_df.shape[0])
+    with col1:
+        st.metric("Majors Rows", majors_df.shape[0])
 
-with col2:
-    st.metric("Majors Columns", majors_df.shape[1])
+    with col2:
+        st.metric("Majors Columns", majors_df.shape[1])
 
 
-with st.expander("Click to see Majors columns"):
-    st.write(majors_df.columns.tolist())
+    with st.expander("Click to see Majors columns"):
+        st.write(majors_df.columns.tolist())
 
-with st.expander("Click to see missing values"):
-    st.write(majors_df.isna().sum())
+    with st.expander("Click to see missing values"):
+        st.write(majors_df.isna().sum())
 
-    st.subheader("Cleaning Steps")
+        st.subheader("Cleaning Steps")
 
     st.write("""
     1. Loaded the College Scorecard dataset.  
